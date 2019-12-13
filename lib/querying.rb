85bd417f60ab810137512786a119286714831f4b
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 03:52:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,6 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  binding.pry
   "SELECT authors.name, subgenres.name
    FROM authors
    JOIN series ON authors.id = series.author_id
