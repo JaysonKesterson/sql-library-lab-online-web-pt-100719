dbd50f8f0ad306df2802826fd66e440d4b1e4e9b
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 03:48:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,6 +23,10 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
+   FROM authors
+   JOIN subgenres
+   ON author.id = series.author_id
+   
   "
 end
 
