2badbdb526cf83193ad5ba2aa4fdbb36f1da4f91
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 04:11:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.names 
    FROM characters
    JOIN characters_books ON characters.id = character_books.character_id
-   JOIN books ON character_books"
+   JOIN books ON character_books.book_id = book.id"
 end
