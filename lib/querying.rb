ed543d7be9de4078d0685dfc9a5df554f6573b26
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 03:59:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
    JOIN characters ON characters.author_id = series.author_id
    GROUP BY species
    ORDER BY COUNT(species)
-   WHERE species = 'human'"
+   HAVING species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
