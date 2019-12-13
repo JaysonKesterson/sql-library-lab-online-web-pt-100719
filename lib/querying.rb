d7debd4e9d541c922482149119381c03ee7c9203
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 03:37:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,8 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
    FROM characters
-   WHERE MAX(COUNT(characters.species))"
+   WHERE MAX(COUNT(characters.species))
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
