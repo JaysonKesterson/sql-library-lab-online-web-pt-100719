b8135c4ac664cc673aaedb936f2bb85ac652b59d
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 03:58:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,8 +33,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title 
    FROM series
-   JOIN authors ON authors.id = series.author_id
-   JOIN characters ON "
+   JOIN characters ON characters.author_id = series.author_id
+   GROUP BY species
+   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
